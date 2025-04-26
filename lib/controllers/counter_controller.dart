494f025177954acbe688f54e2d6da57f35cd851d
Abdunesr <nesredinabdelah33@gmail.com>
2025-04-26 09:48:07 +0300

    managing  a state with getx

---
+++ lib/controllers/counter_controller.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class CounterController extends GetxController {
+  var count = 0.obs; // Observable variable
+
+  void increment() {
+    count++;
+  }
+
+  void decrement() {
+    count--;
+  }
+}
